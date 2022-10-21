5ef8ee0f9fcf9da36241416855ab35c9095fac4e
mudssky <mudssky@gmail.com>
2022-10-21 15:33:50 +0800

    ✨ feat: load env

--- goserver/global/global.go
+++ goserver/global/global.go
@@ -2,10 +2,12 @@ package global
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
+	"github.com/joho/godotenv"
 	"github.com/mudssky/simple-http-file-server/goserver/cmd"
 	"github.com/mudssky/simple-http-file-server/goserver/config"
 	"github.com/spf13/viper"
@@ -25,19 +27,31 @@ func init() {
 	initZap()
 }
 
+// 加载.env文件到全局变量
+func loadDotenv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 func initViper() {
+	osenv, _ := os.LookupEnv("PORT")
+	fmt.Println("env:", osenv)
+	loadDotenv()
 	Viper = viper.GetViper()
 	rootCmd := cmd.InitFlag()
 	viper.BindPFlags(rootCmd.Flags())
 	viper.AddConfigPath(".")
 	viper.SetConfigName("config")
-	if viper.GetString("config") != "" {
-		fmt.Println("命令行设置配置文件：", viper.GetString("config"), viper.AllSettings())
-		viper.SetConfigFile(viper.GetString("config"))
+	if configpath := viper.GetString("config"); configpath != "" {
+		fmt.Println("命令行设置配置文件：", configpath, viper.AllSettings())
+		viper.SetConfigFile(configpath)
+		// ioutil.ReadFile(configpath)
+		// viper.ReadConfig()
 	}
-	// viper.AutomaticEnv()
+	viper.AutomaticEnv()
 	// viper.SetEnvPrefix("GFS")
-	// viper.BindEnv("port")
+	viper.BindEnv("port")
 	err := viper.ReadInConfig() // 查找并读取配置文件
 	if err != nil {             // 处理读取配置文件的错误
 		fmt.Println("未找到配置文件,当前读取到的配置为:", viper.AllSettings())
@@ -48,8 +62,9 @@ func initViper() {
 		fmt.Println("配置文件变动：", e)
 		loadViper()
 		// fmt.Println("当前配置", Config)
-		fmt.Printf("当前配置:%#v", Config)
+		fmt.Printf("当前配置:%+v\n", Config)
 	})
+	fmt.Println("命令行设置配置文件：", viper.AllSettings())
 }
 
 // 当前viper配置加载到结构体
--- goserver/router/index.go
+++ goserver/router/index.go
@@ -4,20 +4,27 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mudssky/simple-http-file-server/goserver/global"
 	"github.com/mudssky/simple-http-file-server/goserver/middleware"
 )
 
 func InitRouter() *gin.Engine {
+	var l = global.Logger
 	var r = gin.New()
 	r.Use(middleware.ZapLogger(), middleware.ZapRecovery(true))
 	// r := gin.Default()
-
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 
 	})
+	// GET local
+	r.GET("/files", func(ctx *gin.Context) {
+		path := ctx.Query("path")
+		l.Debug("path:" + path)
+	})
+
 	return r
 
 }
