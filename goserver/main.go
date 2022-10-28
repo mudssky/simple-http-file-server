bd3a22e6fd1e2349038914a3979cbd1fa4e0ceb6
mudssky <mudssky@gmail.com>
2022-10-28 17:25:15 +0800

    ✨ feat: update goserver

--- goserver/main.go
+++ goserver/main.go
@@ -6,6 +6,22 @@ import (
 	"github.com/mudssky/simple-http-file-server/goserver/util"
 )
 
+// @title           Swagger Example API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.basic  BasicAuth
 func main() {
 	// cmd.Execute()
 	var r = router.InitRouter()
