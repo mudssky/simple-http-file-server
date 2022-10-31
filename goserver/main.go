715353c74801c6f8a6f46a36845ff5f1f7255760
mudssky <mudssky@gmail.com>
2022-10-31 18:29:18 +0800

    ✨ feat: add swagger

--- goserver/main.go
+++ goserver/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	_ "github.com/mudssky/simple-http-file-server/goserver/docs"
 	"github.com/mudssky/simple-http-file-server/goserver/global"
 	"github.com/mudssky/simple-http-file-server/goserver/router"
 	"github.com/mudssky/simple-http-file-server/goserver/util"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 // @title           Swagger Example API
@@ -18,12 +21,13 @@ import (
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      localhost:8080
+// @host      localhost:7888
 // @BasePath  /api/v1
 
 // @securityDefinitions.basic  BasicAuth
 func main() {
 	// cmd.Execute()
 	var r = router.InitRouter()
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.Run(":" + util.Itoa(global.Config.Port)) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
