0468bfb02b865a7f996dfc3b332c592112a7b258
mudssky <mudssky@gmail.com>
2022-11-18 16:43:38 +0800

    ✨ feat: use redux

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,23 +4,32 @@ import { RouterProvider } from 'react-router-dom'
 
 import { ConfigProvider } from 'antd'
 // 由于 antd 组件的默认文案是英文，所以需要修改为中文
-import zhCN from 'antd/es/locale/zh_CN'
-import moment from 'moment'
-import 'moment/locale/zh-cn'
-import 'antd/dist/antd.css'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+import zhCN from 'antd/locale/zh_CN'
+import 'antd/dist/reset.css'
+// 全局css
 import './index.css'
+import './style/scss/common.scss'
+// 全局路由
 import { globalRouter } from './router/routeList'
+// 导入svg配置
 import 'virtual:svg-icons-register'
+// redux配置
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 
-moment.locale('zh-cn')
+dayjs.locale('zh-cn')
 
 ConfigProvider.config({
   prefixCls: 'ant',
 })
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ConfigProvider locale={zhCN}>
-      <RouterProvider router={globalRouter} />
-    </ConfigProvider>
+    <Provider store={store}>
+      <ConfigProvider locale={zhCN}>
+        <RouterProvider router={globalRouter} />
+      </ConfigProvider>
+    </Provider>
   </React.StrictMode>
 )
--- web/src/pages/app/hooks.ts
+++ web/src/pages/app/hooks.ts
@@ -1,24 +1,95 @@
 import { message } from 'antd'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { FileItem, getFileList } from '../../api'
+import { useAppDispatch, useAppSelector } from '../../store/hooks'
+import {
+  BreadcrumbItem,
+  rootBreadcrumbItem,
+  setBreadcrumbitemList,
+  setFileList,
+  setIsNewFolderModalVisible,
+} from '../../store/reducer/homeReducer'
 
 export default function useSetupHook() {
-  const [fileList, setFileList] = useState<FileItem[]>([])
+  const state = useAppSelector((state) => state.home)
+  const dispatch = useAppDispatch()
+  const { fileList, breadcrumbitemList, isNewFolderModalVisible } = state
+
   const getData = async () => {
-    const res = await getFileList()
+    await enterFolder({ path: '' })
+  }
+  const enterFolder = async (
+    params: { path: string },
+    success?: () => void
+  ) => {
+    const res = await getFileList(params)
     console.log('res', res)
     if (res.code === 0) {
       message.success(res.msg)
-      setFileList(res.data)
+      dispatch(setFileList(res.data))
+      success?.()
     } else {
       message.error(res.msg)
     }
+  }
+  const handleFileClick = async (record: FileItem) => {
+    if (!record.isFolder) {
+      message.success('不是文件夹')
+      return
+    }
+    enterFolder({ path: record.path }, () => {
+      dispatch(
+        setBreadcrumbitemList([
+          ...breadcrumbitemList,
+          {
+            key: record.path,
+            name: record.name,
+          },
+        ])
+      )
+    })
+  }
+
+  const handleBreadcrumbJump = async (index: number, item: BreadcrumbItem) => {
+    if (index >= breadcrumbitemList.length - 1 || index < 0) {
+      return
+    }
+    console.log({ index, item })
+
+    await enterFolder({ path: item.key }, () => {
+      if (item.key === rootBreadcrumbItem.key) {
+        dispatch(setBreadcrumbitemList([rootBreadcrumbItem]))
+      } else {
+        // const newlist = [rootBreadcrumbItem]
+        dispatch(setBreadcrumbitemList(breadcrumbitemList.slice(0, index + 1)))
+      }
+    })
+  }
+  const handleBackToParent = async () => {
+    await handleBreadcrumbJump(
+      breadcrumbitemList.length - 2,
+      breadcrumbitemList.at(-2) ?? rootBreadcrumbItem
+    )
+  }
+
+  const showNewFolderModal = () => {
+    dispatch(setIsNewFolderModalVisible(true))
+  }
+  const cancelNewFolderModal = () => {
+    dispatch(setIsNewFolderModalVisible(false))
   }
   useEffect(() => {
     getData()
     return () => {}
   }, [])
   return {
+    breadcrumbitemList,
     fileList,
+    isNewFolderModalVisible,
+    handleFileClick,
+    handleBreadcrumbJump,
+    handleBackToParent,
+    showNewFolderModal,
+    cancelNewFolderModal,
   }
 }
