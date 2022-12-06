99a3de80c89194855aaffc9fcdfcf34b9c959437
mudssky <mudssky@gmail.com>
2022-12-06 16:36:27 +0800

    ✨ feat: drag upload
    drag upload, go gracefullyshutdown

--- goserver/main.go
+++ goserver/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
 	_ "github.com/mudssky/simple-http-file-server/goserver/docs"
 	"github.com/mudssky/simple-http-file-server/goserver/global"
 	"github.com/mudssky/simple-http-file-server/goserver/router"
+	"github.com/mudssky/simple-http-file-server/goserver/server"
 	"github.com/mudssky/simple-http-file-server/goserver/util"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -31,9 +34,22 @@ func main() {
 	// cmd.Execute()
 	var r = router.InitRouter()
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	fmt.Printf(`
 	默认自动化文档地址:http://127.0.0.1:%v/swagger/index.html
 `, global.Config.Port)
-	r.Run(":" + util.Itoa(global.Config.Port)) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 
+	srv := &http.Server{
+		Addr:    ":" + util.Itoa(global.Config.Port),
+		Handler: r,
+	}
+	go func() {
+		// 服务连接
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	// 优雅关机代码,发送 syscall.SIGHUP 信号实现,也就是ctrl+C关闭
+	// 但是watchexec目前不支持这个信号。
+	server.GracefullyShutdown(srv)
 }
