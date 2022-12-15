2b5486283168bff0925167191f2e8a187a4fdf9e
mudssky <mudssky@gmail.com>
2022-12-15 11:31:59 +0800

    :package: build: fix build scripts

--- build.ps1
+++ build.ps1
@@ -12,13 +12,16 @@ Write-Verbose -Message "move to web dir"
 Set-Location web
 Write-Verbose -Message "build web app"
 pnpm build
-if (Test-Path ../goserver/public){
-Write-Verbose -Message "clean goserver/public"
-Remove-Item ..\goserver\public\* -Recurse 
+if (Test-Path ../goserver/public) {
+	Write-Verbose -Message "clean goserver/public"
+	Remove-Item ..\goserver\public\* -Recurse 
+}
+else {
+	mkdir ../goserver/public
 }
 
-Write-Verbose -Message "copy web app"
-Copy-Item  -Force -Path .\dist\  -Destination ..\goserver\public\ -Recurse
+Write-Verbose -Message "move web app"
+Copy-Item  -Path .\dist\*  -Destination ..\goserver\public -Recurse
 Write-Verbose -Message "move to goserver"
 Set-Location ../goserver
 Write-Verbose -Message "build goserver"
