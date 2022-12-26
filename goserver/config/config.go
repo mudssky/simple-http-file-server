5c1d9f1e5b20ec2fd3e38e4b071e6a107aa13076
mudssky <mudssky@gmail.com>
2022-12-26 14:53:32 +0800

    :sparkles: feat: add policy,zap config,config validate

--- goserver/config/config.go
+++ goserver/config/config.go
@@ -1,8 +1,13 @@
 package config
 
 type Server struct {
+	// 配置文件路径
+	Config string `mapstructure:"config"  json:"config" yaml:"config"`
+	// 命令行参数，是否打开浏览器
+	Open          bool `mapstructure:"open" json:"open" yaml:"open"`
 	Port          uint16
 	Zap           Zap           `mapstructure:"zap" json:"zap" yaml:"zap"`
+	Loglevel      string        `mapstructure:"loglevel" json:"loglevel" yaml:"loglevel" validate:"oneof=debug info warn error dpanic panic fatal" `
 	FolderList    []string      `mapstructure:"folderlist" json:"folderlist" yaml:"folderlist"`
 	UserList      []User        `mapstructure:"userlist" json:"userlist" yaml:"userlist"`
 	Jwt           JWT           `mapstructure:"jwt" json:"jwt" yaml:"jwt"`
