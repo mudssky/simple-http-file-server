6dad71e39e69e9ba8df888f4296a7d61a9366675
mudssky <mudssky@gmail.com>
2023-06-10 00:49:58 +0800

    :sparkles: feat: add enum array

---
+++ web/src/util/enumArray2.ts
@@ -0,0 +1,32 @@
+// 枚举类型接口
+export interface EnumArrayObj {
+  value: number | string
+  label: string
+}
+
+export class EnumArray<
+  T extends readonly EnumArrayObj[],
+> extends Array<EnumArrayObj> {
+  constructor(list: T) {
+    super(...list)
+    Object.setPrototypeOf(this, Array.prototype)
+  }
+}
+export const sexEnum = Object.freeze(
+  new EnumArray([
+    {
+      label: '男',
+      value: '1',
+    },
+    {
+      label: '女',
+      value: '2',
+    },
+  ] as const),
+)
+console.log(
+  'test enum filter',
+  sexEnum.filter((item) => {
+    return item.label === '女'
+  }),
+) //TypeError: Spread syntax requires ...iterable[Symbol.iterator] to be a function
