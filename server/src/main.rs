b6db69559d23c435c5a8d4b3188786d76a36eb84
mudssky <mudssky@gmail.com>
2025-03-22 01:01:20 +0800

    build: 更新依赖版本并优化服务器启动代码

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,21 +5,15 @@ pub mod global;
 pub mod router;
 pub mod util;
 use std::net::SocketAddr;
-use tracing::{debug};
-
+use tracing::debug;
 
 /// 项目启动的主函数
 #[tokio::main]
 async fn main() {
     global::init_global_config();
     let app_router = router::init_router();
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    // global::load_env_config();
     debug!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app_router.into_make_service())
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app_router).await.unwrap();
 }
